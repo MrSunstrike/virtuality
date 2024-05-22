fedc7eebe6dd9e0c61f4ba8c8be6f2815aff5434
Mr.Sunstrike <misha@mrsunstrike.ru>
2024-05-22 23:03:42 +0700

    add avatar model

--- virtuality/apps/avatar/models.py
+++ virtuality/apps/avatar/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+from apps.user.models import User
+
+
+class Species(models.Model):
+    pass
+
+
+class Specialization(models.Model):
+    pass
+
+
+class Avatar(models.Model):
+    class AvatarGender(models.TextChoices):
+        MALE = "M", "Самец"
+        FEMALE = "F", "Самка"
+
+    name = models.CharField(
+        verbose_name="Имя",
+        max_length=100,
+    )
+    gender = models.CharField(
+        verbose_name="Пол",
+        max_length=1,
+        choices=AvatarGender
+    )
+    species = models.ForeignKey(
+        to=Species,
+        verbose_name="Вид",
+        on_delete=models.PROTECT,
+        related_name="avatars",
+    )
+    specialization = models.ForeignKey(
+        to=Specialization,
+        verbose_name="Специализация",
+        on_delete=models.PROTECT,
+        related_name="avatars",
+        null=True,
+        blank=True,
+    )
+    owner = models.ForeignKey(
+        to=User,
+        verbose_name="Хозяин",
+        on_delete=models.CASCADE,
+        related_name="avatars",
+    )
