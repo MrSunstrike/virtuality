56e2685939623e9f0c4331dbb265a907b4ab1af4
Mr.Sunstrike <misha@mrsunstrike.ru>
2024-05-24 00:10:59 +0700

    add TextChoices class for stats

--- virtuality/apps/common/models.py
+++ virtuality/apps/common/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Stats(models.TextChoices):
+    MAX_HP = ("max_hp", "Максимальный запас здоровья")
+    MAX_MP = ("max_mp", "Максимальный запас маны")
+    MAX_SP = ("max_sp", "Максимальный запас выносливости")
+    PHYSICAL_DAMAGE = ("phys_dmg", "Физический урон")
+    MAGICAL_DAMAGE = ("mgc_dmg", "Магический урон")
+    PHYSICAL_DEFENSE = ("phys_def", "Защита от физического урона")
+    MAGICAL_DEFENSE = ("mgc_def", "Защита от магического урона")
+    REACTION_SPEED = ("reaction", "Скорость реакции")
+    MOVEMENT_SPEED = ("mv_speed", "Скорость передвижения")
+    CRITICAL_HIT_CHANCE = ("crit_chance", "Шанс на критический удар")
+    PIERCING_CHANCE = ("pierce_chane", "Шанс на пронзающий удар")
+    PHYSICAL_DAMAGE_BLOCK_CHANCE = ("phys_dmg_block_chance", "Шанс на блокирование физического урона")
+    PHYSICAL_DAMAGE_REFLECT_CHANCE = ("phys_dmg_reflect_chance", "Шанс на отражение физического урона")
+    DODGE_CHANCE = ("dodge_chance", "Шанс на уворот от атаки")
+    LUCK = ("luck", "Удача")
